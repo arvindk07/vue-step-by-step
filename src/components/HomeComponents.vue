5b5e45ca1320d4738a79e4fbdc97130d88bbd062
arvindk07 <info.arvind07@gmail.com>
2023-04-28 17:36:10 +0530

    two way binding

--- src/components/HomeComponents.vue
+++ src/components/HomeComponents.vue
@@ -1,31 +1,17 @@
 <template>
-  <h1 v-on:mousemove="getConsole()">Home Components</h1>
-  <button v-on:click="getData('Button 1 CLicked')">Click Me</button>
-  <button @click="getData('Button 2 CLicked')">Click Me</button>
-  <button @dblclick="getData('Button dubble CLicked')">dubble Click Me</button>
+  <h1>Two way binding</h1>
+  <input type="text" v-model="count" />
   <h2>{{ count }}</h2>
-  <button v-on:click="getIncreament()">increament</button>
-  <button v-on:click="count--">increament</button>
 </template>
 
 <script>
 export default {
   name: "HomeComponents",
+
   data() {
     return {
-      count: 0,
+      count: 10,
     };
-  },
-  methods: {
-    getData(data) {
-      alert(data);
-    },
-    getConsole() {
-      console.log("mouse move");
-    },
-    getIncreament() {
-      this.count = this.count + 1;
-    },
   },
 };
 </script>
