6af9a8a53336f889528704d33283b85e04ba85e7
arvindk07 <info.arvind07@gmail.com>
2023-04-28 16:52:29 +0530

    interpolation and data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomeComponents msg="hello world" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeComponents from "./components/HomeComponents.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HomeComponents,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/HomeComponents.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>home page {{ msg }}</h1>
+  <h2>{{ "hello".length }}</h2>
+  <p>{{ email }}</p>
+  <p>{{ (mobile = 9999) }}</p>
+  <p>{{ mobile == 9999 }}</p>
+  <h2>{{ getName("webdev") }}</h2>
+</template>
+
+<script>
+export default {
+  name: "HomeComponents",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      email: "dev@gmail.com",
+      mobile: "32573776",
+      getName: function (a) {
+        return a;
+      },
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: orange;
+}
+</style>
