b56b3f0566ec0cc153d5690134d1f780fe643a0a
arvindk07 <info.arvind07@gmail.com>
2023-04-29 14:09:54 +0530

    Get Checkbox and radio type

--- src/components/HomeComponents.vue
+++ src/components/HomeComponents.vue
@@ -1,10 +1,46 @@
 <template>
-  <h1>Get Input Field Value</h1>
-  <input type="text" placeholder="Enter Email" v-model="email" /> <br />
+  <h1>Get Checkbox and radio type</h1>
+
+  <h3>Technologies</h3>
+  <input
+    type="checkbox"
+    value="frontend Developer"
+    id="frontent"
+    v-model="technology"
+  />
+  <label for="frontent">Frontend Developer</label> <br />
+  <input
+    type="checkbox"
+    value="Backend Developer"
+    id="backend"
+    v-model="technology"
+  />
+  <label for="backend">Backend Developer</label> <br />
+  <input
+    type="checkbox"
+    value="Mern Developer"
+    id="mern"
+    v-model="technology"
+  />
+  <label for="mern">Mern Developer</label>
+
   <br />
-  <input type="password" placeholder="Password" v-model="password" /><br />
   <br />
-  <button v-on:click="getData()" type="button">get values</button>
+  <h3>Radio Selection</h3>
+  <input type="radio" v-model="who" value="student" name="who" id="student" />
+  <label for="student">Student</label><br />
+  <input
+    type="radio"
+    v-model="who"
+    value="developer"
+    name="who"
+    id="developer"
+  />
+  <label for="developer">Developer</label><br />
+  <input type="radio" v-model="who" value="teacher" name="wo" id="teacher" />
+  <label for="teacher">Teacher</label><br />
+  <h4>Selected Technology:{{ technology }}</h4>
+  <h4>Selected Who:{{ who }}</h4>
 </template>
 
 <script>
@@ -12,8 +48,8 @@ export default {
   name: "HomeComponents",
   data() {
     return {
-      email: null,
-      password: null,
+      technology: [],
+      who: [],
     };
   },
 
