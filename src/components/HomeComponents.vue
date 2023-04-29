9d77dc5ab8e80db8b6a2e105799e3c4137a20c98
arvindk07 <info.arvind07@gmail.com>
2023-04-29 14:19:53 +0530

    if else Conditions toggle

--- src/components/HomeComponents.vue
+++ src/components/HomeComponents.vue
@@ -1,46 +1,7 @@
 <template>
-  <h1>Get Checkbox and radio type</h1>
-
-  <h3>Technologies</h3>
-  <input
-    type="checkbox"
-    value="frontend Developer"
-    id="frontent"
-    v-model="technology"
-  />
-  <label for="frontent">Frontend Developer</label> <br />
-  <input
-    type="checkbox"
-    value="Backend Developer"
-    id="backend"
-    v-model="technology"
-  />
-  <label for="backend">Backend Developer</label> <br />
-  <input
-    type="checkbox"
-    value="Mern Developer"
-    id="mern"
-    v-model="technology"
-  />
-  <label for="mern">Mern Developer</label>
-
-  <br />
-  <br />
-  <h3>Radio Selection</h3>
-  <input type="radio" v-model="who" value="student" name="who" id="student" />
-  <label for="student">Student</label><br />
-  <input
-    type="radio"
-    v-model="who"
-    value="developer"
-    name="who"
-    id="developer"
-  />
-  <label for="developer">Developer</label><br />
-  <input type="radio" v-model="who" value="teacher" name="wo" id="teacher" />
-  <label for="teacher">Teacher</label><br />
-  <h4>Selected Technology:{{ technology }}</h4>
-  <h4>Selected Who:{{ who }}</h4>
+  <h1 v-if="show">if else Conditions</h1>
+  <h1 v-else>else Conditions</h1>
+  <button @click="show = !show">toggle</button>
 </template>
 
 <script>
@@ -48,16 +9,10 @@ export default {
   name: "HomeComponents",
   data() {
     return {
-      technology: [],
-      who: [],
+      show: false,
     };
   },
-
-  methods: {
-    getData() {
-      console.log(this.email, this.password);
-    },
-  },
+  methods: {},
 };
 </script>
 
