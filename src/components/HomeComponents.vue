00892b151c110236d115fa50a454e3c78d166ba8
arvindk07 <info.arvind07@gmail.com>
2023-04-30 10:07:22 +0530

    Html Binding

--- src/components/HomeComponents.vue
+++ src/components/HomeComponents.vue
@@ -1,36 +1,20 @@
 <template>
-  <h1>ReUsable Components</h1>
-  <ul>
-    <li v-for="item in users" :key="item.name">
-      <ReusableComponents :data="item" :getData="getData" />
-    </li>
-  </ul>
+  <h1>Html Binding</h1>
+  <h2>{{ text }}</h2>
+  <div v-html="tag"></div>
+  <div v-html="tag2"></div>
 </template>
 
 <script>
-import ReusableComponents from "./ReusableComponents.vue";
 export default {
   name: "HomeComponents",
-  components: {
-    ReusableComponents,
-  },
-
+  components: {},
   data() {
     return {
-      users: [
-        { name: "peter", email: "peter@test.com" },
-        { name: "sam", email: "sam@test.com" },
-        { name: "brad", email: "brad@test.com" },
-        { name: "traversy", email: "traversy@test.com" },
-        { name: "john", email: "john@test.com" },
-      ],
+      text: "Arvind Dev",
+      tag: "<h1>Arvind Hacker</h1>",
+      tag2: "<h1>Kyle is my favourite Youtuber</h1>",
     };
-  },
-
-  methods: {
-    getData(name) {
-      alert(name);
-    },
   },
 };
 </script>
