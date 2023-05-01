4fd10b18cfa3e893b68003fe5173d06ba713d2af
arvindk07 <info.arvind07@gmail.com>
2023-05-01 16:30:10 +0530

    Non Props

---
+++ src/components/ChildVue.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>Dev</h3>
+    <h3>Brad</h3>
+    <h3>Kyle</h3>
+    <h3>{{ data }}</h3>
+  </div>
+  <!-- <h3 v-bind="$attrs">Hello</h3> -->
+</template>
+<script>
+export default {
+  name: "ChildVue",
+  //   inheritAttrs: false,
+  props: {
+    data: String,
+  },
+};
+</script>
--- src/components/HomeComponents.vue
+++ src/components/HomeComponents.vue
@@ -1,17 +1,14 @@
 <template>
-  <h1>Form Modifire</h1>
-  <input type="number" v-model.number.lazy="data" />
-  <p>Data is: {{ typeof data }}</p>
-  <p>Data is: {{ data }}</p>
+  <h1>Non Props</h1>
+  <ChildVue data="data" id="cmp" />
 </template>
 
 <script>
+import ChildVue from "./ChildVue.vue";
 export default {
   name: "HomeComponents",
-  data() {
-    return {
-      data: "",
-    };
+  components: {
+    ChildVue,
   },
 };
 </script>
