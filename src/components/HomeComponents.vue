9f882c0c5d7fc4f91eb08f0350681b157b40de8f
arvindk07 <info.arvind07@gmail.com>
2023-05-01 14:54:23 +0530

    Form Modifire

--- src/components/HomeComponents.vue
+++ src/components/HomeComponents.vue
@@ -1,55 +1,8 @@
 <template>
-  <h1>Simple form</h1>
-  <ul>
-    <li v-for="item in error" :key="item">{{ item }} not valid</li>
-  </ul>
-  <form>
-    <label>Email: </label>
-    <input type="text" placeholder="Enter Email" v-model="form.email" />
-    <br /><br />
-    <label>Password: </label>
-    <input
-      type="password"
-      placeholder="Enter Password"
-      v-model="form.password"
-    />
-    <br />
-    <br />
-    <select v-model="form.country">
-      <option>India</option>
-      <option>USA</option>
-      <option>Astrailiya</option>
-    </select>
-    <br />
-    <br />
-    <h3>Technology</h3>
-    <input type="checkbox" value="react" v-model="form.technology" />
-    <label> React </label>
-    <br />
-    <br />
-    <input type="checkbox" value="vue" v-model="form.technology" />
-    <label> Vue </label>
-    <br />
-    <br />
-    <input type="checkbox" value="next" v-model="form.technology" />
-    <label> Next </label>
-    <br />
-    <br />
-    <input type="checkbox" value="nuxt" v-model="form.technology" />
-    <label> Nuxt </label>
-    <br />
-    <br />
-    <input type="radio" value="male" name="gender" v-model="form.gender" />
-    <label> Male </label>
-    <br />
-    <br />
-    <input type="radio" value="female" name="gender" v-model="form.gender" />
-    <label> Female </label>
-    <br />
-    <br />
-
-    <button v-on:click="login" type="button">Login</button>
-  </form>
+  <h1>Form Modifire</h1>
+  <input type="number" v-model.number.lazy="data" />
+  <p>Data is: {{ typeof data }}</p>
+  <p>Data is: {{ data }}</p>
 </template>
 
 <script>
@@ -57,29 +10,8 @@ export default {
   name: "HomeComponents",
   data() {
     return {
-      form: {
-        email: "",
-        password: "",
-        country: "",
-        technology: [],
-        gender: "",
-      },
-      error: [],
+      data: "",
     };
-  },
-  methods: {
-    login() {
-      this.error = [];
-      for (const item in this.form) {
-        if (this.form[item] === "" || this.form[item.length === 0]) {
-          this.error.push(item);
-        }
-      }
-      if (this.error.length === 0) {
-        alert("data is submited");
-      }
-      console.log("login data: ", this.form, this.error);
-    },
   },
 };
 </script>
